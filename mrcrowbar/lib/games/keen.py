8d027dd09f76250bdb4f9450dac4ee5291299158
Scott Percival <code@moral.net.au>
2016-01-30 17:58:22 +0800

    lib.games.keen: init

---
+++ mrcrowbar/lib/games/keen.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python3
+
+"""File format classes for the Commander Keen: Invasion of the Vorticons engine (DOS, 1991)"""
+
+import itertools
+
+from mrcrowbar import models as mrc
+from mrcrowbar.lib.hardware import ibm_pc
+from mrcrowbar.lib.images import base as img
+from mrcrowbar import utils
+
+# source: http://www.shikadi.net/moddingwiki/Keen_1-3_RLE_compression
+
+class RLECompressor( mrc.Transform ):
+    def import_data( self, buffer ):
+        final_length = utils.from_uint32_le( buffer[0:4] )
+        i = 4
+        out = bytearray()
+        while (len( out ) < final_length):
+            byte = buffer[i]
+            if byte >= 128:
+                out.extend( buffer[i+1:i+byte-126] )
+                i += byte-126
+            else:
+                out.extend( buffer[i+1:i+2]*(byte+3) )
+                i += 2
+
+        result = {
+            'payload': bytes( out ),
+            'end_offset': i
+        }
+
+        return result
+
+# source: http://www.shikadi.net/moddingwiki/RLEW_compression
+
+class RLEWCompressor( mrc.Transform ):
+    def import_data( self, buffer ):
+        final_length = utils.from_uint32_le( buffer[0:4] )
+        i = 4
+        out = bytearray()
+        while (len( out ) < final_length):
+            word = buffer[i:i+2]
+            if word == b'\xfe\xfe':
+                count = utils.from_uint16_le( buffer[i+2:i+4] )
+                data = buffer[i+4:i+6]
+                out.extend( data*count )
+                i += 6
+            else:
+                out.extend( word )
+                i += 2
+
+        result = {
+            'payload': bytes( out ),
+            'end_offset': i
+        }
+
+        return result
+
+
+
+# source: http://www.shikadi.net/moddingwiki/Commander_Keen_EGA_Header
+
+class EGAHead( mrc.Block ):
+    latch_plane_size    = mrc.UInt32_LE( 0x00 )
+    sprite_plane_size   = mrc.UInt32_LE( 0x04 )
+    image_data_start    = mrc.UInt32_LE( 0x08 )
+    sprite_data_start   = mrc.UInt32_LE( 0x0c )
+    font_count          = mrc.UInt16_LE( 0x10 )
+    font_offset         = mrc.UInt32_LE( 0x12 )
+    unknown_1           = mrc.UInt16_LE( 0x16 )
+    unknown_2           = mrc.UInt32_LE( 0x18 )
+    tile_count          = mrc.UInt16_LE( 0x1a )
+    tile_offset         = mrc.UInt32_LE( 0x1c )
+    bitmap_count        = mrc.UInt16_LE( 0x22 )
+    bitmap_offset       = mrc.UInt32_LE( 0x24 )
+    sprite_count        = mrc.UInt16_LE( 0x28 )
+    sprite_offset       = mrc.UInt32_LE( 0x2a )
+    latch_compressed    = mrc.Bits( 0x2e, 0b00000010 )
+    sprite_compressed   = mrc.Bits( 0x2e, 0b00000001 )
+
+
+class EGAHeadSpriteRef( mrc.Block ):
+    width_raw           = mrc.UInt16_LE( 0x00 )
+    height              = mrc.UInt16_LE( 0x02 )
+    prog_offset         = mrc.UInt16_LE( 0x04 )
+    location            = mrc.UInt16_LE( 0x06 )
+    hitbox_left         = mrc.UInt16_LE( 0x08 )
+    hitbox_top          = mrc.UInt16_LE( 0x0a )
+    hitbox_right        = mrc.UInt16_LE( 0x0c )
+    httbox_bottom       = mrc.UInt16_LE( 0x0e )
+    name                = mrc.CStringN( 0x10, 12 )
+    horiz_offset        = mrc.UInt16_LE( 0x1c )
+    vert_offset         = mrc.UInt16_LE( 0x1e )
+
+
+
+class PreviewCompressor( mrc.Transform ):
+    rle = RLECompressor()
+    plan = img.Planarizer( 320, 200, 4, plane_padding=1536 ) 
+    
+    def import_data( self, buffer ):
+        assert type( buffer ) == bytes
+        stage_1 = self.rle.import_data( buffer )
+        stage_2 = self.plan.import_data( stage_1['payload'] )
+        result = {
+            'payload': stage_2['payload'],
+            'end_offset': stage_1['end_offset']
+        }
+        return result
+
+
+class Preview( mrc.Block ):
+    image               = mrc.BlockField( img.RawIndexedImage, 0x0000, block_kwargs={ 'width': 320, 'height': 200, 'palette': ibm_pc.EGA_DEFAULT_PALETTE }, transform=PreviewCompressor() )
+
+
+class PreviewFile( mrc.Block ):
+    data                = mrc.BlockField( Preview, 0x0000, transform=RLECompressor() )
+
+
+# source: http://www.shikadi.net/moddingwiki/Commander_Keen_1-3_Level_format
+
+class Level( mrc.Block ):
+    pass
+
+
+class LevelHeader( mrc.Block ):
+    width           = mrc.UInt16_LE( 0x00 )
+    height          = mrc.UInt16_LE( 0x02 )
+    plane_count     = mrc.Check( mrc.UInt16_LE( 0x04 ), 2 )
+    plane_size      = mrc.UInt16_LE( 0x0e )
+
+
+class LevelTile( mrc.Block ):
+    tile_id         = mrc.UInt16_LE( 0x00 )
+
+
+class KeenLoader( mrc.Loader ):
+    KEEN_FILE_CLASS_MAP = {
+        '/FINALE.CK[1-3]$': Preview,
+        '/PREVIEW[2-3].CK1': Preview
+    }
+
+    def __init__( self ):
+        super( Loader, self ).__init__( self.KEEN_FILE_CLASS_MAP )

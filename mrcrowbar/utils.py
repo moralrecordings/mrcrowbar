8af98474a8979d08981e1bca2a8f3b429542c802
Scott Percival <code@moral.net.au>
2016-07-18 20:53:09 +0800

    add pylint checks

--- mrcrowbar/utils.py
+++ mrcrowbar/utils.py
@@ -1,72 +1,68 @@
 import struct
 
-from_byte_type =    lambda format, size, buffer: struct.unpack( format, buffer[:size] )[0]
-to_byte_type =      lambda format, value: struct.pack( format, value )
+def _from_byte_type( code, size ):
+    return lambda buffer: struct.unpack( code, buffer[:size] )[0]
 
-from_int8 =         lambda buffer: from_byte_type( '<b', 1, buffer )
-from_uint8 =        lambda buffer: from_byte_type( '>B', 1, buffer )
-from_uint16_le =    lambda buffer: from_byte_type( '<H', 2, buffer )
-from_uint32_le =    lambda buffer: from_byte_type( '<I', 4, buffer )
-from_uint64_le =    lambda buffer: from_byte_type( '<Q', 8, buffer )
-from_int16_le =     lambda buffer: from_byte_type( '<h', 2, buffer )
-from_int32_le =     lambda buffer: from_byte_type( '<i', 4, buffer )
-from_int64_le =     lambda buffer: from_byte_type( '<q', 8, buffer )
-from_float_le =     lambda buffer: from_byte_type( '<f', 4, buffer )
-from_double_le =    lambda buffer: from_byte_type( '<d', 8, buffer )
-from_uint16_be =    lambda buffer: from_byte_type( '>H', 2, buffer )
-from_uint32_be =    lambda buffer: from_byte_type( '>I', 4, buffer )
-from_uint64_be =    lambda buffer: from_byte_type( '>Q', 8, buffer )
-from_int16_be =     lambda buffer: from_byte_type( '>h', 2, buffer )
-from_int32_be =     lambda buffer: from_byte_type( '>i', 4, buffer )
-from_int64_be =     lambda buffer: from_byte_type( '>q', 8, buffer )
-from_float_be =     lambda buffer: from_byte_type( '>f', 4, buffer )
-from_double_be =    lambda buffer: from_byte_type( '>d', 8, buffer )
+def _to_byte_type( code ):
+    return lambda value: struct.pack( code, value )
 
-to_int8 =           lambda value: to_byte_type( '<b', value )
-to_uint8 =          lambda value: to_byte_type( '>B', value )
-to_uint16_le =      lambda value: to_byte_type( '<H', value )
-to_uint32_le =      lambda value: to_byte_type( '<I', value )
-to_uint64_le =      lambda value: to_byte_type( '<Q', value )
-to_int16_le =       lambda value: to_byte_type( '<h', value )
-to_int32_le =       lambda value: to_byte_type( '<i', value )
-to_int64_le =       lambda value: to_byte_type( '<q', value )
-to_float_le =       lambda value: to_byte_type( '<f', value )
-to_double_le =      lambda value: to_byte_type( '<d', value )
-to_uint16_be =      lambda value: to_byte_type( '>H', value )
-to_uint32_be =      lambda value: to_byte_type( '>I', value )
-to_uint64_be =      lambda value: to_byte_type( '>Q', value )
-to_int16_be =       lambda value: to_byte_type( '>h', value )
-to_int32_be =       lambda value: to_byte_type( '>i', value )
-to_int64_be =       lambda value: to_byte_type( '>q', value )
-to_float_be =       lambda value: to_byte_type( '>f', value )
-to_double_be =      lambda value: to_byte_type( '>d', value )
+BYTE_TYPES = {
+    'int8':         ('<b', 1),
+    'uint8':        ('>B', 1),
+    'uint16_le':    ('<H', 2),
+    'uint32_le':    ('<I', 4),
+    'uint64_le':    ('<Q', 8),
+    'int16_le':     ('<h', 2),
+    'int32_le':     ('<i', 4),
+    'int64_le':     ('<q', 8),
+    'float_le':     ('<f', 4),
+    'double_le':    ('<d', 8),
+    'uint16_be':    ('>H', 2),
+    'uint32_be':    ('>I', 4),
+    'uint64_be':    ('>Q', 8),
+    'int16_be':     ('>h', 2),
+    'int32_be':     ('>i', 4),
+    'int64_be':     ('>q', 8),
+    'float_be':     ('>f', 4),
+    'double_be':    ('>d', 8),
+}
+
+def _load_byte_types():
+    for byte_type, (type_code, type_size) in BYTE_TYPES.items():
+        globals()['from_{}'.format(byte_type)] = _from_byte_type( type_code, type_size )
+        globals()['to_{}'.format(byte_type)] = _to_byte_type( type_code )
+
+_load_byte_types()
 
 
 def unpack_bits(byte):
     """Expand a bitfield into a 64-bit int (8 bool bytes)"""
-    y =          byte & (0x00000000000000ff)
-    y = (y | (y<<28)) & (0x0000000f0000000f)
-    y = (y | (y<<14)) & (0x0003000300030003)
-    y =  (y | (y<<7)) & (0x0101010101010101)
-    return y
+    longbits = byte & (0x00000000000000ff)
+    longbits = (longbits | (longbits<<28)) & (0x0000000f0000000f)
+    longbits = (longbits | (longbits<<14)) & (0x0003000300030003)
+    longbits = (longbits | (result<<7)) & (0x0101010101010101)
+    return longbits
 
 
 def pack_bits(longbits):
     """Crunch a 64-bit int (8 bool bytes) into a bitfield"""
-    y =      longbits & (0x0101010101010101)
-    y =  (y | (y>>7)) & (0x0003000300030003)
-    y = (y | (y>>14)) & (0x0000000f0000000f)
-    y = (y | (y>>28)) & (0x00000000000000ff)
-    return y
+    byte = longbits & (0x0101010101010101)
+    byte = (byte | (byte>>7)) & (0x0003000300030003)
+    byte = (byte | (byte>>14)) & (0x0000000f0000000f)
+    byte = (byte | (byte>>28)) & (0x00000000000000ff)
+    return byte
 
 
 class BitReader( object ):
     def __init__( self, buffer, start_offset, bytes_reverse=False, bits_reverse=False ):
-        assert type( buffer ) == bytes
+        assert isinstance( buffer, bytes )
+        assert start_offset in range( len( buffer ) )
         self.buffer = buffer
         self.bits_reverse = bits_reverse
         self.bytes_reverse = bytes_reverse
-        self.set_offset( start_offset )
+        self.pos = start_offset
+        self.bits_remaining = 8
+        self.current_bits = self.buffer[self.pos]
 
 
     def set_offset( self, offset ):
@@ -76,9 +72,9 @@ class BitReader( object ):
         self.current_bits = self.buffer[self.pos]
 
 
-    def get_bits( self, n ):
+    def get_bits( self, count ):
         result = 0
-        for i in range( n ):
+        for _ in range( count ):
             if self.bits_remaining <= 0:
                 new_pos = self.pos + (-1 if self.bytes_reverse else 1)
                 if new_pos not in range( len( self.buffer ) ):
@@ -94,7 +90,7 @@ class BitReader( object ):
             else:
                 bit = (self.current_bits & 1)
                 self.current_bits >>= 1
-            
+
             self.bits_remaining -= 1
 
             result <<= 1
@@ -112,8 +108,8 @@ class BitWriter( object ):
         self.current_bits = 0
 
 
-    def put_bits( self, value, n ):
-        for i in range( n ):
+    def put_bits( self, value, count ):
+        for _ in range( count ):
 
             # bits are retrieved from the source LSB first
             bit = (value & 1)
@@ -143,7 +139,7 @@ class BitWriter( object ):
 
     def get_buffer( self ):
         last_byte = self.current_bits if (self.bits_remaining < 8) else None
-            
+
         result = self.output
         if last_byte:
             result = bytearray( result )

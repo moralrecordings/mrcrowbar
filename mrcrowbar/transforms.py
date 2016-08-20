c082beded31603696428068aab63d7694892164a
Scott Percival <code@moral.net.au>
2016-08-20 12:23:49 +0800

    transforms: pylint scrubbing

--- mrcrowbar/transforms.py
+++ mrcrowbar/transforms.py
@@ -1,9 +1,29 @@
+"""Definition classes for transformations."""
 
 class Transform( object ):
+    """Base class for defining transformations."""
+    # pylint: disable=unused-argument,no-self-use
+
     def export_data( self, buffer, parent=None ):
+        """Perform a transform on a byte string.
+
+        buffer
+            Source byte string.
+
+        parent
+            Parent object of the source (to provide context for Refs).
+        """
         return None
-    
+
     def import_data( self, buffer, parent=None ):
+        """Perform a reverse-transform on a byte string.
+
+        buffer
+            Source byte string.
+
+        parent
+            Parent object of the source (to provide context for Refs).
+        """
         return {
             'payload': b'',
             'end_offset': 0

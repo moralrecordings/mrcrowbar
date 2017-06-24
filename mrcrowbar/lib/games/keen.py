c7a76e0c769e02abfded5ebffb855ba8b136e74f
Scott Percival <code@moral.net.au>
2017-06-24 11:50:29 +0800

    lib.games.keen: stub out more formats

--- mrcrowbar/lib/games/keen.py
+++ mrcrowbar/lib/games/keen.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
 
-"""File format classes for the Commander Keen: Invasion of the Vorticons engine (DOS, 1991)"""
+"""File format classes for the Commander Keen: Invasion of the Vorticons engine (DOS, 1991)
+
+Sources:
+RLE compressor
+http://www.shikadi.net/moddingwiki/Keen_1-3_RLE_compression
+
+RLEW compressor
+http://www.shikadi.net/moddingwiki/RLEW_compression
+
+LZW compressor
+http://www.shikadi.net/moddingwiki/LZW_Compression
+(Special thanks to Fleexy)
+
+EGA header
+http://www.shikadi.net/moddingwiki/Commander_Keen_EGA_Header
+
+Level format
+http://www.shikadi.net/moddingwiki/Commander_Keen_1-3_Level_format
+"""
 
 import itertools
 
@@ -9,7 +27,6 @@ from mrcrowbar.lib.hardware import ibm_pc
 from mrcrowbar.lib.images import base as img
 from mrcrowbar import utils
 
-# source: http://www.shikadi.net/moddingwiki/Keen_1-3_RLE_compression
 
 class RLECompressor( mrc.Transform ):
     def import_data( self, buffer, parent=None ):
@@ -32,7 +49,6 @@ class RLECompressor( mrc.Transform ):
 
         return result
 
-# source: http://www.shikadi.net/moddingwiki/RLEW_compression
 
 class RLEWCompressor( mrc.Transform ):
     def import_data( self, buffer, parent=None ):
@@ -58,20 +74,79 @@ class RLEWCompressor( mrc.Transform ):
         return result
 
 
+class LZWCompressor( mrc.Transform ):
 
-# source: http://www.shikadi.net/moddingwiki/Commander_Keen_EGA_Header
+    def import_data( self, buffer, parent=None ):
+        decomp_size = utils.from_uint32_le( buffer[:4] )
+        max_bits = utils.from_uint16_le( buffer[4:6] )      # should be 12
 
-class EGAHead( mrc.Block ):
+        lookup = [bytes((i,)) for i in range( 256 )]
+        lookup.append( None )   # 256: error
+        lookup.append( None )   # 257: end of data
+
+        output = bytearray()
+
+        bs = utils.BitReader( buffer, 6, bits_reverse=True, output_reverse=True )
+        state = {'usebits': 9}
+
+        def add_to_lookup( state, entry ):
+            if len( lookup ) < (1 << max_bits):
+                #print('lookup[{}] = {}'.format( len( lookup ), entry ) )
+                lookup.append( entry )
+                if len( lookup ) == (1 << state['usebits'])-1:
+                    state['usebits'] = min( state['usebits']+1, max_bits )
+                    #print('usebits = {}'.format(state['usebits']))
+            return
+                    
+
+        fcode = bs.get_bits( state['usebits'] )
+        match = lookup[fcode]
+        #print( 'fcode={},match={}'.format( fcode, match ) )
+        output.extend( match )
+        while True:
+            ncode = bs.get_bits( state['usebits'] )
+            #print( 'ncode={}'.format( ncode ) )
+            if ncode == 257:
+                # end of data
+                break
+            elif ncode == 256:
+                # error
+                raise Exception( 'Found error code, data is not valid' )
+            elif ncode < len( lookup ):
+                nmatch = lookup[ncode]
+            else:
+                nmatch = match+match[0:1]
+            #print( 'match={}'.format(match) )
+            #print( 'nmatch={}'.format(nmatch) )
+            output.extend( nmatch )
+
+            # add code to lookup 
+            add_to_lookup( state, match+nmatch[0:1] )
+            match = nmatch
+
+        if len( output ) != decomp_size:
+            print( 'Warning: was expecting data of size {}, got data of size {} instead'.format( decomp_size, len( output ) ) )
+
+        result = {
+            'payload': bytes( output ),
+            'end_offset': bs.pos
+        }
+
+        return result
+
+
+
+class EGAHeader( mrc.Block ):
     latch_plane_size    = mrc.UInt32_LE( 0x00 )
     sprite_plane_size   = mrc.UInt32_LE( 0x04 )
     image_data_start    = mrc.UInt32_LE( 0x08 )
     sprite_data_start   = mrc.UInt32_LE( 0x0c )
-    font_count          = mrc.UInt16_LE( 0x10 )
-    font_offset         = mrc.UInt32_LE( 0x12 )
-    unknown_1           = mrc.UInt16_LE( 0x16 )
-    unknown_2           = mrc.UInt32_LE( 0x18 )
-    tile_count          = mrc.UInt16_LE( 0x1a )
-    tile_offset         = mrc.UInt32_LE( 0x1c )
+    tile8_count         = mrc.UInt16_LE( 0x10 )
+    tile8_offset        = mrc.UInt32_LE( 0x12 )
+    tile32_count        = mrc.UInt16_LE( 0x16 )
+    tile32_offset       = mrc.UInt32_LE( 0x18 )
+    tile16_count        = mrc.UInt16_LE( 0x1c )
+    tile16_offset       = mrc.UInt32_LE( 0x1e )
     bitmap_count        = mrc.UInt16_LE( 0x22 )
     bitmap_offset       = mrc.UInt32_LE( 0x24 )
     sprite_count        = mrc.UInt16_LE( 0x28 )
@@ -80,14 +155,14 @@ class EGAHead( mrc.Block ):
     sprite_compressed   = mrc.Bits( 0x2e, 0b00000001 )
 
 
-class EGAHeadBitmapRef( mrc.Block ):
+class EGAHeaderBitmapRef( mrc.Block ):
     width_raw           = mrc.UInt16_LE( 0x00 )
     height              = mrc.UInt16_LE( 0x02 )
     location            = mrc.UInt32_LE( 0x04 )
-    name                = mrc.CStringN( 0x08, 8 )
+    name                = mrc.Bytes( 0x08, length=8 )
 
 
-class EGAHeadSpriteRef( mrc.Block ):
+class EGAHeaderSpriteRef( mrc.Block ):
     width_raw           = mrc.UInt16_LE( 0x00 )
     height              = mrc.UInt16_LE( 0x02 )
     prog_offset         = mrc.UInt16_LE( 0x04 )
@@ -96,13 +171,49 @@ class EGAHeadSpriteRef( mrc.Block ):
     hitbox_top          = mrc.UInt16_LE( 0x0a )
     hitbox_right        = mrc.UInt16_LE( 0x0c )
     httbox_bottom       = mrc.UInt16_LE( 0x0e )
-    name                = mrc.CStringN( 0x10, 12 )
+    name                = mrc.Bytes( 0x10, length=12 )
     horiz_offset        = mrc.UInt16_LE( 0x1c )
     vert_offset         = mrc.UInt16_LE( 0x1e )
 
     @property
     def location( self ):
         return self.location_raw*16
+
+
+
+class EGATiles( mrc.Block ):
+    pass
+#    tile_data           =   mrc.Bytes(
+#                                0x00, transform=img.Planarizer(
+#                                    width=32,
+#                                    height,
+#                                    bpp=4,
+#                                    frame_count
+#                                )
+#                            )
+    
+
+class EGALatch( mrc.Block ):
+    tiles               = mrc.BlockField( EGATiles, 0x00 )
+
+
+class EGALatchComp( mrc.Block ):
+    tiles               = mrc.BlockField( EGATiles, 0x00, transform=LZWCompressor() )
+
+
+class SoundHeader( mrc.Block ):
+    magic               = mrc.Const( mrc.Bytes( 0x00, length=4 ), b'SND\x00' )
+    size                = mrc.UInt16_LE( 0x04 )
+    unknown_1           = mrc.UInt16_LE( 0x06 )
+    count               = mrc.UInt16_LE( 0x08 )
+    padding             = mrc.Const( mrc.Bytes( 0x0a, length=6 ), b'\x00'*6 )
+
+
+class SoundRef( mrc.Block ):
+    offset              = mrc.UInt16_LE( 0x00 )
+    priority            = mrc.UInt8( 0x02 )
+    rate                = mrc.UInt8( 0x03 )
+    name                = mrc.Bytes( 0x04, 0x0c )
 
 
 class PreviewCompressor( mrc.Transform ):
@@ -138,22 +249,56 @@ class Level( mrc.Block ):
 class LevelHeader( mrc.Block ):
     width           = mrc.UInt16_LE( 0x00 )
     height          = mrc.UInt16_LE( 0x02 )
-    plane_count     = mrc.Check( mrc.UInt16_LE( 0x04 ), 2 )
+    plane_count     = mrc.UInt16_LE( 0x04, default=2 )
+    unknown_1       = mrc.Bytes( 0x06, length=4 )
+    ted_vars        = mrc.Bytes( 0x0a, length=4 )
     plane_size      = mrc.UInt16_LE( 0x0e )
+    unknown_2       = mrc.Bytes( 0x10, length=16 )
+
+    @property
+    def plane_size_calc( self ):
+        size = 2 * self.width * self.height
+        return size + ((16-size)%16)
 
 
 class LevelTile( mrc.Block ):
     tile_id         = mrc.UInt16_LE( 0x00 )
 
 
+class ScoresItems( mrc.Block ):
+    joystick        = mrc.UInt16_LE( 0x00, range( 0, 1 ) )
+    battery         = mrc.UInt16_LE( 0x02, range( 0, 1 ) )
+    vacuum          = mrc.UInt16_LE( 0x04, range( 0, 1 ) )
+    liquor          = mrc.UInt16_LE( 0x06, range( 0, 1 ) )
+
+
+class ScoresName( mrc.Block ):
+    name            = mrc.Bytes( 0x00, length=13 )
+
+
+class Scores( mrc.Block ):
+    values          = mrc.UInt32_LE( 0x00, length=7 )
+    items           = mrc.BlockField( ScoresItems, 0x1c, count=7 )
+    num_cities      = mrc.UInt16_LE( 0x54, length=7, range=range( 0, 9 ) )
+    unknown_1       = mrc.Bytes( 0x62, length=14 )
+    names           = mrc.BlockField( ScoresName, 0x70, count=7 )
+    term            = mrc.Const( mrc.Bytes( 0xcb, length=1 ), b'\x00' )
+
+
 class Loader( mrc.Loader ):
     _SEP = mrc.Loader._SEP
 
     _KEEN_FILE_CLASS_MAP = {
-        _SEP+'(EGAHEAD).CK[1-3]$': EGAHead,
+        _SEP+'(EGAHEAD).CK[1-3]$': EGAHeader,
+        _SEP+'(EGALATCH).CK1$': EGALatchComp,
+        _SEP+'(EGALATCH).CK[2-3]$': EGALatch,
         _SEP+'(FINALE).CK[1-3]$': Preview,
-        _SEP+'(PREVIEW)([2-3]).CK1': Preview
+        _SEP+'(LEVEL)([0-9]{2}).CK[1-3]$': Level,
+        _SEP+'(PREVIEW)([2-3]).CK[1-3]$': Preview,
+        _SEP+'(SCORES).CK[1-3]$': Scores,
     }
+
+
 
     def __init__( self ):
         super( Loader, self ).__init__( self._KEEN_FILE_CLASS_MAP )

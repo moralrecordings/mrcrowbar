04b8ce0df223e0df88ecd31aba2c30e863e64106
Scott Percival <code@moral.net.au>
2018-08-04 19:41:30 +0800

    lib.platforms.director: add more types

--- mrcrowbar/lib/platforms/director.py
+++ mrcrowbar/lib/platforms/director.py
@@ -4,6 +4,8 @@ from mrcrowbar.lib.images import base as img
 from mrcrowbar.lib.containers import riff
 from mrcrowbar import utils
 
+from enum import IntEnum
+
 DIRECTOR_PALETTE_RAW =  '000000111111222222444444555555777777888888aaaaaa'\
                         'bbbbbbddddddeeeeee000011000022000044000055000077'\
                         '0000880000aa0000bb0000dd0000ee001100002200004400'\
@@ -72,6 +74,10 @@ class BitmapCastV4( mrc.Block ):
     unk4 = mrc.Bytes( 0x1a, length=0x24 )
     name = mrc.Bytes( 0x3e )
 
+    @property
+    def repr( self ):
+        return 'name={}, pitch={}, bpp={}, reg_x={}, reg_y={}, unk1={}, unk2={}'.format( self.name, self.pitch, self.bpp, self.reg_x, self.reg_y, self.unk1, self.unk2 )
+
 
 class BitmapCompressor( mrc.Transform ):
     def import_data( self, buffer ):
@@ -89,13 +95,76 @@ class BitmapCompressor( mrc.Transform ):
                 result.extend( buffer[pointer:pointer+length] )
                 pointer += length
         return {'payload': result}
-        
+
+
+class CastType( IntEnum ):
+    NULL =          0x00
+    BITMAP =        0x01
+    FILM_LOOP =     0x02
+    TEXT =          0x03
+    PALETTE =       0x04
+    PICTURE =       0x05
+    SOUND =         0x06
+    BUTTON =        0x07
+    SHAPE =         0x08
+    MOVIE =         0x09
+    DIGITAL_VIDEO = 0x0a
+    LINGO_SCRIPT =  0x0b
+    RTE =           0x0c
+
 
 class CastV4( mrc.Block ):
-    size1 = mrc.UInt16_BE( 0x00 )
-    size2 = mrc.UInt32_BE( 0x02 )
-    cast_type = mrc.UInt8( 0x06 )
+    size1 =     mrc.UInt16_BE( 0x00 )
+    size2 =     mrc.UInt32_BE( 0x02 )
+    cast_type = mrc.UInt8( 0x06, enum=CastType )
 
+    @property
+    def repr( self ):
+        return 'size1: {}, size2: {}, type: {}'.format( self.size1, self.size2, str( self.cast_type ) )
+
+
+class KeyEntry( mrc.Block ):
+    section_index   = mrc.UInt32_BE( 0x00 )
+    cast_index      = mrc.UInt32_BE( 0x04 )
+    chunk_id        = mrc.Bytes( 0x08, length=4 )
+
+    @property
+    def repr( self ):
+        return 'chunk_id: {}, section_index: {}, cast_index: {}'.format( self.chunk_id, self.section_index, self.cast_index )
+
+
+class KeyV4( mrc.Block ):
+    unk1 =      mrc.UInt16_BE( 0x00 )
+    unk2 =      mrc.UInt16_BE( 0x02 )
+    unk3 =      mrc.UInt32_BE( 0x04 )
+    entry_count =  mrc.UInt32_BE( 0x08 )
+    entries =       mrc.BlockField( KeyEntry, 0x0c, count=mrc.Ref( 'entry_count' ) )
+
+
+class MMapEntry( mrc.Block ):
+    chunk_id =  mrc.Bytes( 0x00, length=4 )
+    length =    mrc.UInt32_BE( 0x04 )
+    offset =    mrc.UInt32_BE( 0x08 )
+    flags =     mrc.UInt16_BE( 0x0c )
+    unk1 =      mrc.UInt16_BE( 0x0e )
+    memsize =   mrc.UInt32_BE( 0x10 )
+
+    @property
+    def repr( self ):
+        return 'chunk_id: {}, length: 0x{:08x}, offset: 0x{:08x}, flags: {}'.format( self.chunk_id, self.length, self.offset, self.flags )
+
+
+class MMapV4( mrc.Block ):
+    unk1 =      mrc.Bytes( 0x00, length=8 )
+    entries_max = mrc.UInt32_BE( 0x04 )
+    entries_used = mrc.UInt32_BE( 0x08 )
+    unk2 =      mrc.Const( mrc.Bytes( 0x0c, length=8 ), b'\xff'*8 )
+    unk3 =      mrc.UInt32_BE( 0x14 )
+    entries =    mrc.BlockField( MMapEntry, 0x18, count=mrc.Ref( 'entries_max' ), fill=b'\xaa'*0x14 )
+    
+    @property
+    def repr( self ):
+        return 'entries_max: {}, entries_used: {}'.format( self.entries_max, self.entries_used )
 
 
 class DIR( riff.RIFX ):

96e4b512dd0797805b7d0b1e9a60eaecef1e392a
Scott Percival <code@moral.net.au>
2018-08-30 01:09:39 +0800

    models: add NumberField_P definitions

--- mrcrowbar/models.py
+++ mrcrowbar/models.py
@@ -6,11 +6,16 @@ from mrcrowbar.refs import Ref, ConstRef, property_get, property_set, \
 from mrcrowbar.fields import ParseError, FieldValidationError, Field, \
                             ChunkField, BlockField, Bytes, \
                             CString, CStringN, CStringNStream, NumberField, \
-                            Int8, UInt8, Bits, UInt16_LE, UInt32_LE, \
-                            UInt64_LE, Int16_LE, Int32_LE, Int64_LE, \
-                            Float32_LE, Float64_LE, UInt16_BE, UInt32_BE, \
-                            UInt64_BE, Int16_BE, Int32_BE, Int64_BE, \
-                            Float32_BE, Float64_BE
+                            Int8, UInt8, Bits, \
+                            Int16_LE, Int32_LE, Int64_LE, \
+                            UInt16_LE, UInt32_LE, UInt64_LE, \
+                            Float32_LE, Float64_LE, \
+                            Int16_BE, Int32_BE, Int64_BE, \
+                            UInt16_BE, UInt32_BE, UInt64_BE, \
+                            Float32_BE, Float64_BE, \
+                            Int16_P, Int32_P, Int64_P, \
+                            UInt16_P, UInt32_P, UInt64_P, \
+                            Float32_P, Float64_P
 from mrcrowbar.blocks import Block, Unknown
 from mrcrowbar.loaders import Loader
 from mrcrowbar.transforms import Transform

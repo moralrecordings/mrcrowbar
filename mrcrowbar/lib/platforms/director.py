034a39fdd093f806c4a072ea7f35a23b37e59aea
Scott Percival <code@moral.net.au>
2018-10-01 23:27:47 +0800

    lib.platforms.director: fix palette, add sound and script casts

--- mrcrowbar/lib/platforms/director.py
+++ mrcrowbar/lib/platforms/director.py
@@ -40,7 +40,7 @@ DIRECTOR_PALETTE_RAW =  '000000111111222222444444555555777777888888aaaaaa'\
                         'ff9966ff9999ff99ccff99ffffcc00ffcc33ffcc66ffcc99'\
                         'ffccccffccffffff00ffff33ffff66ffff99ffffccffffff'
 
-DIRECTOR_PALETTE = img.from_palette_bytes( bytes.fromhex( DIRECTOR_PALETTE_RAW ), stride=3, order=(0, 1, 2) )
+DIRECTOR_PALETTE = [p for p in reversed( img.from_palette_bytes( bytes.fromhex( DIRECTOR_PALETTE_RAW ), stride=3, order=(0, 1, 2) ) )]
 
 
 class SoundV4( mrc.Block ):
@@ -105,47 +105,20 @@ class SoundCastV4( mrc.Block ):
             ', name={}'.format( self.extra[0].name ) if self.extra else '' )
 
 
-class Rect( mrc.Block ):
-    top = mrc.UInt16_BE( 0x00 )
-    left = mrc.UInt16_BE( 0x02 )
-    bottom = mrc.UInt16_BE( 0x04 )
-    right = mrc.UInt16_BE( 0x06 )
-
-    @property
-    def width( self ):
-        return self.right-self.left
-
-    @property
-    def height( self ):
-        return self.bottom-self.top
+class ScriptCastV4( mrc.Block ):
+    unk1        = mrc.Bytes( 0x00, length=0x15 )
+    script_id   = mrc.UInt16_BE( 0x15 )
+    unk4_size   = mrc.UInt16_BE( 0x17 )
+    unk3        = mrc.UInt32_BE( 0x19 )
+    unk4        = mrc.UInt32_BE( 0x1d, count=mrc.Ref( 'unk4_size' ) )
 
     @property
     def repr( self ):
-        return 'top={}, left={}, bottom={}, right={}, width={}, height={}'.format( 
-            self.top, self.left, self.bottom, self.right, self.width, self.height )
-
-
-class BitmapCastV4( mrc.Block ):
-    unk1 = mrc.UInt8( 0x00 )
-    unk2 = mrc.Bits( 0x01, 0xf0 )
-    pitch = mrc.Bits( 0x01, 0x0fff, size=2 )
-    initial_rect = mrc.BlockField( Rect, 0x03 )
-    bounding_rect = mrc.BlockField( Rect, 0x0b )
-    reg_x = mrc.UInt16_BE( 0x13 )
-    reg_y = mrc.UInt16_BE( 0x15 )
-    #bpp = mrc.UInt16_BE( 0x17 )
-    #unk4 = mrc.Bytes( 0x1a, length=0x24 )
-    #name = mrc.Bytes( 0x3e )
-    unk4 = mrc.Bytes( 0x17 )
-
-    @property
-    def repr( self ):
-        #return 'name={}, pitch={}, bpp={}, reg_x={}, reg_y={}, unk1={}, unk2={}'.format( self.name, self.pitch, self.bpp, self.reg_x, self.reg_y, self.unk1, self.unk2 )
-        return 'pitch={}, reg_x={}, reg_y={}, unk1={}, unk2={}, unk4={}'.format( self.pitch, self.reg_x, self.reg_y, self.unk1, self.unk2, self.unk4 )
+        return 'script_id={}'.format( self.script_id )
 
 
 class BitmapCompressor( mrc.Transform ):
-    def import_data( self, buffer ):
+    def import_data( self, buffer, parent=None ):
         result = bytearray()
         pointer = 0
         while (pointer < len( buffer )):
@@ -162,6 +135,57 @@ class BitmapCompressor( mrc.Transform ):
         return {'payload': result}
 
 
+class BitmapV4( mrc.Block ):
+    data = mrc.Bytes( 0x00 )
+
+
+class Rect( mrc.Block ):
+    top = mrc.Int16_BE( 0x00 )
+    left = mrc.Int16_BE( 0x02 )
+    bottom = mrc.Int16_BE( 0x04 )
+    right = mrc.Int16_BE( 0x06 )
+
+    @property
+    def width( self ):
+        return self.right-self.left
+
+    @property
+    def height( self ):
+        return self.bottom-self.top
+
+    @property
+    def repr( self ):
+        return 'top={}, left={}, bottom={}, right={}, width={}, height={}'.format( 
+            self.top, self.left, self.bottom, self.right, self.width, self.height )
+
+
+class BitmapCastV4( mrc.Block ):
+    _data = None
+
+    unk1 = mrc.UInt8( 0x00 )
+    bpp = mrc.Bits( 0x01, 0xf0 )
+    pitch = mrc.Bits( 0x01, 0x0fff, size=2 )
+    initial_rect = mrc.BlockField( Rect, 0x03 )
+    bounding_rect = mrc.BlockField( Rect, 0x0b )
+    reg_x = mrc.UInt16_BE( 0x13 )
+    reg_y = mrc.UInt16_BE( 0x15 )
+    #bpp = mrc.UInt16_BE( 0x17 )
+    #unk4 = mrc.Bytes( 0x1a, length=0x24 )
+    #name = mrc.Bytes( 0x3e )
+    unk4 = mrc.Bytes( 0x17 )
+
+    @property
+    def repr( self ):
+        #return 'name={}, pitch={}, bpp={}, reg_x={}, reg_y={}, unk1={}, unk2={}'.format( self.name, self.pitch, self.bpp, self.reg_x, self.reg_y, self.unk1, self.unk2 )
+        return 'bpp={}, pitch={}, reg_x={}, reg_y={}, initial_rect={}, bounding_rect={}'.format( self.bpp, self.pitch, self.reg_x, self.reg_y, self.initial_rect, self.bounding_rect )
+
+    def __init__( self, *argc, **argv ):
+        self.image = img.IndexedImage( self, mrc.Ref( '_data.data' ), mrc.Ref( 'pitch' ), mrc.Ref( 'initial_rect.height' ), palette=DIRECTOR_PALETTE )
+        super().__init__( *argc, **argv )
+
+
+
+
 class CastType( IntEnum ):
     NULL =          0x00
     BITMAP =        0x01
@@ -173,20 +197,23 @@ class CastType( IntEnum ):
     BUTTON =        0x07
     SHAPE =         0x08
     MOVIE =         0x09
-    DIGITAL_VIDEO = 0x0a
-    LINGO_SCRIPT =  0x0b
+    VIDEO =         0x0a
+    SCRIPT =        0x0b
     RTE =           0x0c
 
 
 class CastV4( mrc.Block ):
     CAST_MAP = {
         CastType.BITMAP: BitmapCastV4,
+        CastType.SOUND: SoundCastV4,
+        CastType.SCRIPT: ScriptCastV4,
     }
 
     size1 =     mrc.UInt16_BE( 0x00 )
     size2 =     mrc.UInt32_BE( 0x02 )
     cast_type = mrc.UInt8( 0x06, enum=CastType )
     detail =    mrc.BlockField( CAST_MAP, 0x07, block_type=mrc.Ref( 'cast_type' ), default_klass=mrc.Unknown )
+    garbage =   mrc.Bytes( mrc.EndOffset( 'detail' ) )
 
     @property
     def repr( self ):
@@ -253,6 +280,7 @@ class DirectorV4Map( riff.RIFXMap ):
         riff.Tag( b'Sord' ): Sord,
         riff.Tag( b'CASt' ): CastV4,
         riff.Tag( b'snd ' ): SoundV4,
+        riff.Tag( b'BITD' ): BitmapV4,
     }
 DirectorV4Map.CHUNK_MAP[riff.Tag( b'RIFX' )] = DirectorV4Map
 
@@ -269,7 +297,7 @@ class PJ93( mrc.Block ):
     fontmap_offset = mrc.UInt32_P( 0x08 )
     resfork1_offset = mrc.UInt32_P( 0x0c )
     resfork2_offset = mrc.UInt32_P( 0x10 )
-    graphics_dll_offset = mrc.UInt32_P( 0x14 )
-    sound_dll_offset = mrc.UInt32_P( 0x18 )
+    dirdib_drv_offset = mrc.UInt32_P( 0x14 )
+    macromix_dll_offset = mrc.UInt32_P( 0x18 )
     rifx_offset_dup = mrc.UInt32_P( 0x1c )
-    
+    unk1 = mrc.Bytes( 0x20, 0xc ) 
